67e675069f9f3942389a6062f5988e6ae6a3294f
Atte Juvonen <atte.juvonen@futurice.com>
2019-01-24 18:15:27 +0200

    Added generated static files

---
+++ public/component---src-pages-404-js-7d83d5eaa36182cf8c96.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{299:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a);t.default=function(){return u.a.createElement("div",null,u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-7d83d5eaa36182cf8c96.js.map
