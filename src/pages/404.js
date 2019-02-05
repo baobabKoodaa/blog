65c804d483b333c75cd6409a4db3ec4ef9ab167b
Atte Juvonen <atte.juvonen@futurice.com>
2019-02-05 11:01:32 +0200

    Bugfix: 404 page text is no longer hidden under header.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,25 @@
+import PropTypes from "prop-types";
 import React from "react";
+import { ThemeContext } from "../layouts";
+import Article from "../components/Article";
+import Headline from "../components/Article/Headline";
 
-const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
-);
+const NotFoundPage = () => {
+
+  return (
+    <React.Fragment>
+      <ThemeContext.Consumer>
+        {theme => (
+          <Article theme={theme}>
+            <header>
+              <Headline title="404" theme={theme} />
+            </header>
+            <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+          </Article>
+        )}
+      </ThemeContext.Consumer>
+    </React.Fragment>
+  );
+};
 
 export default NotFoundPage;
