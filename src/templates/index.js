fe876dbf41f59d63973a5b89b0c6d19ba728a7a1
Atte Juvonen <atte.juvonen@futurice.com>
2019-02-26 21:55:49 +0200

    Infinite scroll: hide pagination when JS is enabled.

--- src/templates/index.js
+++ src/templates/index.js
@@ -13,19 +13,32 @@ import { connectGeoSearch } from "react-instantsearch-dom";
 /** Template for "home" page with infinite scroll and fallback to pagination. */
 class IndexPage extends React.Component {
 
-  // State is used for Infinite Scroll and initialized properly in componentDidMount.
   state = {
-    items: [], // Items which should be rendered based on how low the user has scrolled
-    isLoading: true, // Avoid triggering multiple simultaenous loadings
-    cursor: -42 // Represents next page which infinite scroll should fetch. 
+    /*  items contains posts which should be rendered
+        items is initialized to 1 page of results, in order to:
+          1. render a page to users who have JS disabled
+          2. render initial paint fast for all users
+        the initial page depends on pageContext.currentPage (corresponds to a path like "/", "/2", "/3",...)
+    */
+    items: this.props.data.posts.edges,
+    /*
+        isLoading is used to avoid triggering multiple simultaenous loadings
+    */
+    isLoading: true,
+    /*
+        cursor represents next page which infinite scroll should fetch
+    */
+    cursor: this.props.pageContext.currentPage+1,
+    /*
+        showPaginationLinks is used as fallback in case of errors or non JS users
+    */
+    showPaginationLinks: true
   }
 
   componentDidMount() {
-    // Load initial items fast
     this.setState(state => ({
-      items: this.props.data.posts.edges,
-      isLoading: false,
-      cursor: this.props.pageContext.currentPage+1
+      isLoading: false, // Allow triggering infinite scroll load
+      showPaginationLinks: false // Hide pagination links for users who are running JS // TODO somehow do this faster?
     }))
 
     // If you want to do paginated fetch for first items, uncomment the following line:
@@ -41,14 +54,17 @@ class IndexPage extends React.Component {
       .then(
         res => {
           this.setState(state => ({
-            items: [...state.items, ...res], // Add resulting edges to state.items
+            items: [...state.items, ...res], // Add resulting post items to state.items
             cursor: state.cursor+1, // Update which page should be fetched next
             isLoading: false // Loading is complete so a new load can be triggered.
           }))
         },
         error => {
-          // TODO make pagination visible in case of error?
-          this.setState({ isLoading: false, error })
+          this.setState({
+            isLoading: false,
+            error,
+            showPaginationLinks: true // Fallback to pagination on error, too.
+          })
         }
     )
   }
@@ -56,56 +72,59 @@ class IndexPage extends React.Component {
   render() {
 
     return (
-      <React.Fragment>
-        <ThemeContext.Consumer>
-          {theme =>
-            <React.Fragment>
+      <ThemeContext.Consumer>
+        {theme =>
+          <React.Fragment>
 
-              {/* Uncomment this to get Hero section.
-              {this.props.pageContext.currentPage == 1 && (
-                <Hero theme={theme} />
-              )} */}
+            {/* Uncomment this to get Hero section.
+            {this.props.pageContext.currentPage == 1 && (
+              <Hero theme={theme} />
+            )} */}
 
-              <div>
-                <InfiniteScroll
-                throttle={100}
-                threshold={300}
-                isLoading={this.state.isLoading}
-                hasMore={this.state.cursor <= this.props.pageContext.numPages}
-                onLoadMore={this.loadMore}
-                >
-                  <Blog posts={this.state.items} theme={theme} />
+            {/* Blog posts with infinite scroll */}
+            <InfiniteScroll
+              throttle={100}
+              threshold={300}
+              isLoading={this.state.isLoading}
+              hasMore={this.state.cursor <= this.props.pageContext.numPages}
+              onLoadMore={this.loadMore}
+            >
+                <Blog posts={this.state.items} theme={theme} />
+            </InfiniteScroll>
 
-                </InfiniteScroll>
-                {this.state.isLoading && (
-                  <div className="spinner">
-                    <FaCog/>
-                  </div>
-                  
-                )}
+            {/* Show loading spinner if user scrolls too fast. */}
+            {this.state.isLoading && !this.state.showPaginationLinks && (
+              <div className="spinner">
+                <FaCog/>
               </div>
+            )}
+
+            {/* Fallback to Pagination Links */}
+            {this.state.showPaginationLinks && 
               <Pagination pageContext={this.props.pageContext} theme={theme}/>
-              <Seo pageTitle="Blog home"/>
-              <style jsx>{`
-                @keyframes spinner {
-                  to {transform: rotate(360deg);}
+            }
+
+            <Seo pageTitle="Blog home"/>
+
+            <style jsx>{`
+              @keyframes spinner {
+                to {transform: rotate(360deg);}
+              }
+              .spinner {
+                margin-top: 40px;
+                font-size: 60px;
+                text-align: center;
+                :global(svg) {
+                  fill: ${theme.color.brand.primaryLight};
+                  animation: spinner 3s linear infinite;
                 }
-                .spinner {
-                  margin-top: 40px;
-                  font-size: 60px;
-                  text-align: center;
-                  :global(svg) {
-                    fill: ${theme.color.brand.primaryLight};
-                    animation: spinner 3s linear infinite;
-                  }
-                  
-                }
-                        
-              `}</style>
-            </React.Fragment>
-          }
-        </ThemeContext.Consumer>
-      </React.Fragment>
+                
+              }
+                      
+            `}</style>
+          </React.Fragment>
+        }
+      </ThemeContext.Consumer>
     );
   }
 }
