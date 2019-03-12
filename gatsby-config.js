7a18e5cd9d8b17d25769ea01eb557e215c86db58
Atte Juvonen <atte.juvonen@futurice.com>
2019-03-12 22:54:48 +0200

    Removed gatsby-plugin-catch-links now that we can use re-link.

--- gatsby-config.js
+++ gatsby-config.js
@@ -169,7 +169,6 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-catch-links`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
