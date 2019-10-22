b54954b42bd8c68aceba4c4d4c0e099d6f58d47f
Luis Salas <luisalas@gmail.com>
2019-10-21 20:37:50 -0500

    Cambio en Post 2 y recomendaciones ESLint

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,7 +70,7 @@ module.exports = {
         isTSX: true, // defaults to false
         //jsxPragma: `jsx`, // defaults to "React" ??
         allExtensions: true
-      },
+      }
     },
     {
       resolve: `gatsby-plugin-layout`,
@@ -94,7 +94,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/${process.env.POSTS_FOLDER || 'mock_posts'}/`,
+        path: `${__dirname}/content/${process.env.POSTS_FOLDER || "mock_posts"}/`,
         name: "posts"
       }
     },
@@ -126,18 +126,18 @@ module.exports = {
             options: {
               maxWidth: 800,
               backgroundColor: "transparent",
-              tracedSVG: { color: '#f9ebd2' }
+              tracedSVG: { color: "#f9ebd2" }
             }
           },
           {
             resolve: `gatsby-remark-rehype-images`,
             options: {
-              tag: 're-img',
+              tag: "re-img",
               maxWidth: 800,
               quality: 90,
               webP: true,
-              toFormat: 'WEBP',
-              tracedSVG: { color: '#f9ebd2' },
+              toFormat: "WEBP",
+              tracedSVG: { color: "#f9ebd2" },
               generateTracedSVG: true
             }
           },
