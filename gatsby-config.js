1516a882ccf7bb2c9fb507c154c19c07f31f4fe7
Luis F. Salas <luisalas@gmail.com>
2019-11-09 20:28:55 -0500

    add GTM plugin & GTM ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -118,6 +118,27 @@ module.exports = {
             }
           },
           {
+            resolve: "gatsby-plugin-google-tagmanager",
+            options: {
+              id: "YGTM-M3JD7MS",
+        
+              // Include GTM in development.
+              // Defaults to false meaning GTM will only be loaded in production.
+              includeInDevelopment: false,
+        
+              // datalayer to be set before GTM is loaded
+              // should be an object or a function that is executed in the browser
+              // Defaults to null
+              defaultDataLayer: { platform: "gatsby" },
+        
+              // Specify optional GTM environment details.
+              gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+              gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+              dataLayerName: "YOUR_DATA_LAYER_NAME",
+            },
+          },
+        
+          {
             resolve: "gatsby-remark-component-parent2div",
             options: { components: ["re-icons", "re-img", "re-tracedsvg-gallery"] }
           },
