6ee93fe3d70716e570c43e3c5ae6b40fa9a94591
Atte Juvonen <git23123761293@baobab.fi>
2020-12-01 23:17:12 +0200

    Purged Google Analytics. Begone, demon, I command you!

--- gatsby-config.js
+++ gatsby-config.js
@@ -121,12 +121,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID
-      }
-    },
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: config.manifestName,
